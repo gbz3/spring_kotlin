fbcdf92d3e47ada634016c16fe7dcc22aabec492
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-08-08 11:26:46 +0900

    File upload

---
+++ src/main/kotlin/com/example/postal_code/controller/WebController.kt
@@ -0,0 +1,31 @@
+package com.example.postal_code.controller
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
+import java.io.Serializable
+
+@Controller
+class WebController {
+
+    val log: Logger = LoggerFactory.getLogger(WebController::class.java)
+
+    @GetMapping("/upload")
+    fun viewUpload(@ModelAttribute("form") form: FileUploadForm): String {
+        return "upload"
+    }
+
+    @PostMapping("/upload")
+    fun upload(@ModelAttribute("form") form: FileUploadForm): String {
+        log.trace("upload size=${form.uploadFile?.size}")
+        return "uploaded"
+    }
+
+    data class FileUploadForm(var uploadFile: MultipartFile?)
+
+}
