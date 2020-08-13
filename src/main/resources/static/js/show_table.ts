e917773ac5a6863220b0285fb5a01db1dbb18eeb
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-08-13 11:43:12 +0900

    郵便番号上二桁をリストボックスに出力

--- src/main/resources/static/js/show_table.ts
+++ src/main/resources/static/js/show_table.ts
@@ -14,18 +14,26 @@ class Address {
     kanjiCho: string
 }
 
-let data: Array<Address> = []
+let data: Address[] = []
 
-async function loadPostalCode() {
-    const response = await fetch('http://localhost:8080/api/table/postal_code')
-    return await response.json() as Array<Address>
+async function loadData(): Promise<void> {
+    if (data === null || data.length === 0) {
+        const response = await fetch('http://localhost:8080/api/table/postal_code')
+        data = await response.json() as Address[]
+    }
 }
 
 export async function getTableBody(f1, f2 = (a, b) => Number(a.zip) - Number(b.zip)): Promise<string> {
-    data = await loadPostalCode()
+    await loadData()
     const tbody = data.filter(f1)
         .sort(f2)
         .map(addr => `<tr><td>${addr.zip}</td><td>${addr.kanjiKen}</td><td>${addr.kanjiShi}</td><td>${addr.kanjiCho}</td></tr>`)
         .join('')
     return `<thead><th>zip</th><th>kanjiKen</th><th>kanjiShi</th><th>kanjiCho</th></thead><tbody>${tbody}</tbody>`
+}
+
+export async function getKenOptions(): Promise<string> {
+    await loadData()
+    const kens = Array.from(new Set(data.map(addr => `${addr.zip.substring(0, 2)}${addr.kanjiKen}`))).sort()
+    return kens.map(k => `<option value="${k.substring(0, 2)}">${k.substring(2)} (${k.substring(0, 2)})</option>`).join()
 }
